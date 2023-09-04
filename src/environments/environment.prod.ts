9830e17d3c7e2c57f05041287635c3932ba48414
Mannulus <pipe_050401@hotmail.com>
2023-09-04 01:40:05 -0500

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   adminService: 'http://feliperojas.live/smart-campus/admin',
-  dataService: 'http://feliperojas.live/smart-campus/data',
+  dataService: 'http://feliperojas.live/smart-campus/data/api',
   brokerUrl: '13.82.121.101',
   brokerPort: 61616,
   brokerPath: '', //optional
