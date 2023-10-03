319171a47c62675dda12311d2a0e2c0bda774805
FelipeRojas <andres2180021@correo.uis.edu.co>
2023-10-03 17:05:10 -0500

    update enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,8 @@
 export const environment = {
   production: false,
-  adminService: 'http://feliperojas.live/smart-campus/admin',
-  dataService: 'http://localhost:8091/api',
+  adminService: 'http://localhost:8090/admin',
+  dataService: 'http://localhost:8091/data/api',
   brokerUrl: '13.82.121.101',
   brokerPort: 61614,
   brokerPath: '', //optional
-  //front de autenticacion
-  urlFrontAuth: 'http://localhost:4200',
-  //front de administracion
-  urlFrontAdmin: 'http://localhost:4300',
 };
